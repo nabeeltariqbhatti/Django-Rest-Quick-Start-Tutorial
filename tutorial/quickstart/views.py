52c7fbc24e6d95b0db967a0db08f259d54328d3b
Nabeel Tariq Bhatti <nabeeltaariq@gmail.com>
2021-04-01 10:02:40 +0500

    permisiion

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -10,7 +10,7 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated,permissions.IsAdminUser]
+    permission_classes = [permissions.IsAuthenticated]
 
 
 
